bc58f4ba150f7f0215865076d8ccfef978f0e1f9
Adan Altamira <adanaltamira@Adans-MacBook-Pro.local>
2015-01-15 22:42:04 -0600

    initial game display

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,26 @@
-var app = function() {
-  options: {
-    one: 'rock',
-    two: 'paper',
-    three: 'scissors'
+'use strict';
+
+var App = function() {
+  var options = ['rock', 'paper', 'scissors'];
+
+  // Retun a number between 0-2 to match element in options
+  function getRandomInt() {
+    return Math.floor(Math.random() * (4 - 1) + 1);
   }
+
+  // attach click to trigger element
+  $(document).on('click', '#trigger', function(e) {
+    e.preventDefault();
+
+    var $current = $(e.currentTarget);
+
+    $current.attr('disabled', 'disabled');
+
+    $('#result-player-one').text(options[getRandomInt() - 1]);
+    $('#result-player-two').text(options[getRandomInt() - 1]);
+
+    $current.removeAttr('disabled');
+  });
 };
+
+new App();
